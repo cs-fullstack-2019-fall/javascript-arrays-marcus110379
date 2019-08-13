919c528a58deeae2d294b760bf264876186bbe88
marcus110379 <marcusjmadden@gmail.com>
2019-08-13 12:11:40 -0500

    graded

---
+++ index.js
@@ -0,0 +1,52 @@
+// alert("Test");
+// ### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the ar
+// ### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the array
+
+
+ let names = [];
+names.push("matthew");
+names.push("april");
+names.push("marty");
+names.push("tony");
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
+
+//
+// #### Exercise 2
+// 1. Create an array with 5 numbers in it.
+// 2. Print the numbers.
+
+// let numbers = [ 2,4,6,7,9];
+// for(let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+//
+//
+// ### Exercise 3
+// 1. Create an array with 4 names.
+// 2. Print the third item in the array.
+// 3. Delete the second item.
+// 4. Print the third item in the array again.
+
+// let names = [ "Marcus","Paul", "John", "Mellow"];
+// console.log(names[2]);
+// names.splice(1,1);
+// console.log(names);
+//
+
+
+
+
+
+
+
+
